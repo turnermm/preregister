61ddd2fa002e5df5b5a5f956a1951af03427a1d0
Myron Turner <turnermm02@shaw.ca>
2013-05-11 22:01:01 -0500

    update to header info

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * artistsinf plugin
+ * pregister plugin
  * registers users by means of a confirmation link 
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Myron Turner<turnermm02@shaw.ca>
