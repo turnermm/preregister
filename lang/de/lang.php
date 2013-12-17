46dfa727a7345da9033f8f10badca5f17a36e64a
Egmont Schreiter <egmont.schreiter@gmx.de>
2013-12-17 07:10:06 +0100

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,11 +4,13 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Matthias Schulte <dokuwiki@lupo49.de>
+ * @author Egmont Schreiter <egmont.schreiter@gmx.de>
  */
 $lang['old_confirmation']      = 'Diese Bestätigungs-ID ist nicht mehr gültig. Der Benutzer wurde ggf. bereits angelegt.';
 $lang['bad_email']             = 'Ungültige E-Mail-Adresse:';
 $lang['uid_inuse']             = 'Die Benutzer-ID ist bereits in Benutzung:';
 $lang['registering']           = 'Registrierung:';
+$lang['cards_nomatch']         = 'Ihre Auswahl passt nicht zu den Karten, bitte erneut versuchen.';
 $lang['confirmation']          = 'Es wurde eine E-Mail mit einem Bestätigungs-Link an Ihre E-Mail-Adresse versendet. Nach Aufruf des Links wird die Registrierung abgeschlossen und Sie erhalten Ihr Passwort.';
 $lang['email_problem']         = 'Es ist ein Fehler beim Versenden des Bestätigungs-Links an Ihre E-Mail-Adresse aufgetreten. Bitte später erneut probieren.';
 $lang['screen_confirm']        = 'Bitte auf den oben angegebenen Link klicken, um die Registrierung zu bestätigen. Anschließend erhalten Sie per E-Mail Ihr Kennwort.';
