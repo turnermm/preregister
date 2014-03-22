65d4b0ad818f9436612138d184258dec2b64c6bf
rene <wllywlnt@yahoo.com>
2014-03-22 09:40:34 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author rene <wllywlnt@yahoo.com>
+ */
+$lang['old_confirmation']      = 'Dit bevestigings id is niet langer geschikt.De gebruiker is waarschijnlijk inmiddels toegevoegd.';
+$lang['bad_email']             = 'Ongeldig e-mail adres:';
+$lang['uid_inuse']             = 'userid is in gebruik:';
+$lang['registering']           = 'Registreren:';
+$lang['cards_nomatch']         = 'Je keuze stemt niet overeen met de kaarten, probeer het nogmaals.';
+$lang['confirmation']          = 'Een email met een bevestigingslink werd verzonden aan het opgegeven adres. Klik op de link of plak het in de browsernavigatiebalk. Daarna ben je geregistreerd en ontvang je een wachtwoord.';
+$lang['email_problem']         = 'Er was een probleem bij het verzenden van de bevestigings e-mail. Probeer het later nog eens.';
+$lang['screen_confirm']        = 'Klik op de bovenstaande link om de registratie te bevestigen. Daarna ontvang je een email met je wachtwoord.';
+$lang['email_confirm']         = 'Klik op de link of plak het in de browsernavigatiebalk om de registratie af te ronden.';
+$lang['subject_confirm']       = 'Bevestigings Link van:';
+$lang['menu']                  = 'Lege Preregister bevestigings lijst';
+$lang['btn_confirm']           = 'Bevestig verwijderen';
+$lang['btn_secure']            = 'Beveiligd bestand';
+$lang['invalid']               = 'onjuiste invoer gedetecteerd!';
---
+++ lang/nl/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author rene <wllywlnt@yahoo.com>
+ */
+$lang['captcha']               = 'CAPTCHA. Keuzen zijn  \'none\', \'builtin\', en \'captcha plugin\', Indien \'captcha plugin\' wordt geselecteerd dan moet deze geïnstalleerd zijn; indien niet gevonden dan wordt deze vervangen.';
+$lang['send_confirm']          = 'Stuur bevestigings link naar het e-mailadres. Indien onwaar zal de link worden getoond. ';
+$lang['list_age']              = 'Tijdsbestek om niet geactiveerde bevestigings gegevens te bewaren. Standaard is dit twee dagen (2*24*60*60).Verwijderingen worden via het administratie scherm van de plugin uitgevoerd.';
