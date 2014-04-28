054150c91be8c0655cfdac1f48c9b0c817d5fb95
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-04-28 21:40:32 +0200

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['old_confirmation']      = 'Cette confirmation n\'est plus valide. Il se peut que le compte d\'utilisateur ait été créé.';
+$lang['bad_email']             = 'adresse de courriel invalide:';
+$lang['uid_inuse']             = 'identifiant déjà utilisé:';
+$lang['registering']           = 'Inscription en cours:';
+$lang['cards_nomatch']         = 'Vos choix ne correspondent pas aux cartes; veuillez essayer à nouveau.';
+$lang['confirmation']          = 'Un courriel contenant un lien de confirmation vient de vous être expédié. Veuillez suivre ce lien ou le copier dans votre navigateur. Votre inscription sera alors effective et vous recevrez votre mot de passe';
+$lang['email_problem']         = 'Erreur lors de l\'envoi du lien de confirmation à votre adresse de courriel. Veuillez réessayer ultérieurement.';
+$lang['screen_confirm']        = 'Veuillez cliquer sur le lien ci-dessus pour confirmer votre inscription. Vous recevrez alors votre mot de passe par courriel.';
+$lang['email_confirm']         = 'Veuillez suivre ce lien ou le copier dans votre navigateur pour compléter votre inscription :';
+$lang['subject_confirm']       = 'Lien de confirmation de :';
+$lang['menu']                  = 'Nettoyer la liste de préinscription';
+$lang['btn_confirm']           = 'Confirmer les suppressions';
+$lang['btn_secure']            = 'Sécuriser le fichier de données';
+$lang['invalid']               = 'Entrée invalide détectée !';
---
+++ lang/fr/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['captcha']               = 'CAPTCHA. Les choix possibles sont "none" (aucun), "builtin" (interne) et "captcha plugin" (extension captcha). Pour choisir "extension captcha", il faut que cette dernière soit installée; si elle n\'est pas détectée, la valeur "interne" sera utilisée à la place.';
+$lang['send_confirm']          = 'Envoyer le lien de confirmation à l\'adresse de courriel. Si désactivé, le lien s\'affiche à l\'écran.';
+$lang['list_age']              = 'Durée de conservation en secondes des données de confirmation qui n\'ont pas été activées. La valeur par défaut est deux jours (2*24*60*60). Le panneau d\'administration permet d\'effectuer des suppressions.';
