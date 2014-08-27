f5e02e9b3c5bdb5bc1d58cd72e8caa07c121e8b7
Felipe Castro <fefcas@gmail.com>
2014-08-27 13:50:33 +0200

    translation update

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Felipe Castro <fefcas@gmail.com>
+ */
+$lang['captcha']               = 'CAPTCHA. Escolhas são \'none\', \'builtin\', e \'captcha plugin\'. Se for selecionado \'captcha plugin\' então ele deve estar instalado; se ele não for encontrado o \'bultin\' será o substituto.';
+$lang['send_confirm']          = 'Enviar linque de confirmação para endereço de emeil. Se não estiver ativado, o linque será mostrado na tela.';
+$lang['list_age']              = 'Tempo para salvar entradas de confirmação que não foram ativadas. Por padrão usa-se dois dias (2*24*60*60). Exclusões são feitas pelo painel de administração da extensão.';
