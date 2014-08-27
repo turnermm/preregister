e5f0b925d5eb2444c12df63076c8ca1fb142344f
Felipe Castro <fefcas@gmail.com>
2014-08-27 13:35:07 +0200

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Felipe Castro <fefcas@gmail.com>
+ */
+$lang['captcha']               = 'CAPTCHA. Elektoj estas \'none\', \'builtin\', kaj \'captcha plugin\'; se \'captcha plugin\' estas elektata, do ĝi devas esti instalita; se ĝi ne estos trovata, la \'builtin\' anstataŭos.';
+$lang['send_confirm']          = 'Sendi konfirman ligon al retadreso. Se malaktiva, la ligo estos montrata sur la ekrano.';
+$lang['list_age']              = 'Kvanto da tempo por konservi konfirmaj enigoj kiuj ne estis aktivataj. Apriore ni uzas du tagojn (2*24*60*60). Forigoj estas plenumataj per la administrilo de la kromaĵo.';
