9d14639a60e3f63d39b0acf39fa74355c359f22d
Hideaki SAWADA <chuno@live.jp>
2015-07-13 09:40:07 +0200

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['captcha']               = 'CAPTCHA。選択肢は \'none\' \'builtin\' \'captcha plugin\'。\'captcha plugin\' を選択する場合、captcha プラグインがインストールしていないのに \'captcha plugin\' を選択した場合、\'builtin\' に置換えます。';
+$lang['send_confirm']          = 'メールアドレスに確認用リンクを送信します。無効にすると確認用リンクを画面に表示します。';
+$lang['list_age']              = '未確認入力を保持する時間の長さ。デフォルトは２日間（2*24*60*60)。削除はプラグインの管理画面を使って下さい。';
