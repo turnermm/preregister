501cc7a08cae249d982a9e9c27be6f8cef564c39
Tom Richter <tom.richter@outlook.com>
2017-09-20 12:13:24 -0400

    Added recaptcha2 to localized admin panel text

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -5,6 +5,6 @@
  * 
  * @author Felipe Castro <fefcas@gmail.com>
  */
-$lang['captcha']               = 'CAPTCHA. Escolhas são \'none\', \'builtin\', e \'captcha plugin\'. Se for selecionado \'captcha plugin\' então ele deve estar instalado; se ele não for encontrado o \'bultin\' será o substituto.';
+$lang['captcha']               = 'CAPTCHA. Escolhas são \'none\', \'builtin\', \'captcha plugin\', e \'recaptcha2\'. Se for selecionado \'captcha plugin\' ou \'recaptcha2\' então ele deve estar instalado; se ele não for encontrado o \'bultin\' será o substituto.';
 $lang['send_confirm']          = 'Enviar linque de confirmação para endereço de emeil. Se não estiver ativado, o linque será mostrado na tela.';
 $lang['list_age']              = 'Tempo para salvar entradas de confirmação que não foram ativadas. Por padrão usa-se dois dias (2*24*60*60). Exclusões são feitas pelo painel de administração da extensão.';
